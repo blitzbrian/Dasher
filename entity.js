0e3b195e867825b1bc69fdd92d1a353411880d6a
dazerstudio <brianvoorbij@gmail.com>
2021-06-14 20:55:07 +0200

    Commit

--- entity.js
+++ entity.js
@@ -1,14 +1,14 @@
 class Player {
   constructor() {
     this.animationIndex = 0;
-    this.pos = createVector(0, height >= 500 ? 350 : height - 150);
+    this.pos = createVector(0, height >= 500 ? 355 : height - 145);
     this.vel = createVector();
     this.acc = createVector();
   }
 
   update() {
     this.cameraOffset = width / 6;
-    this.animationIndex += 0.0075 * deltaTime;
+    this.animationIndex += 0.01 * deltaTime;
     if (!grid.isThere(this.pos.x + this.cameraOffset, this.pos.y + 50)) this.acc.y += 0.005 * deltaTime;
     else this.acc.y = 0;
     if (grid.isThere(this.pos.x + this.cameraOffset, this.pos.y + 50) && (keyIsDown(32) || mouseIsPressed)) this.acc.y = -7.5;
